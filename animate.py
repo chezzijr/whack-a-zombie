f3f9fa410f8d9fb41664f30ae84849025767cdfb
chezzijr <nhuthuan0501@gmail.com>
2025-01-12 20:57:16 +0700

    time animation which cause frame stays for a duration before advance to next frame

--- animate.py
+++ animate.py
@@ -25,3 +25,22 @@ class Animation:
 
     def reset(self) -> None:
         self.current_frame = 0
+
+# so that animations can be played at any frame rate
+class TimeAnimation(Animation):
+    def __init__(self, frames: list[pygame.Surface], frame_duration: float, repeat: bool = True):
+        super().__init__(frames, repeat)
+        self.frame_duration = frame_duration
+        self.elapsed_time = 0
+        self.curr_frame = super().get_first_frame()
+
+    def next_frame_with_dt(self, dt: float, flip: bool = False) -> pygame.Surface | None:
+        self.elapsed_time += dt
+        if self.elapsed_time >= self.frame_duration:
+            self.elapsed_time -= self.frame_duration
+            self.curr_frame = super().next_frame(flip)
+        return self.curr_frame
+
+    def reset(self) -> None:
+        self.elapsed_time = 0
+        super().reset()
